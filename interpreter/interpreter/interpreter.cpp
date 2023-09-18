ec754559d731a74f8560b0b55a8bd7c9a4fc2ebb
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-09-18 16:08:03 +0200

    interp

--- interpreter/interpreter/interpreter.cpp
+++ interpreter/interpreter/interpreter.cpp
@@ -1,22 +1,57 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 int main() {
-    std::ifstream plik("main.txt");
 
-    if (!plik.is_open()) {
+    std::ifstream file("main.txt");
+    std::vector<std::string> commands;
+
+    if (!file.is_open()) {
+
         std::cerr << "Error opening file" << std::endl;
         return 1;
     }
 
-    std::string slowo;
+    std::string com;
+    while (file >> com) {
 
-    while (plik >> slowo) {
-        std::cout << slowo << std::endl;
+        commands.push_back(com);
     }
 
-    plik.close();
+    for (int i = 0; i < commands.size(); i++) {
 
+        if (commands[i] == "out" && commands[i + 2] == "->") {
+
+            std::cout << commands[i + 1];
+        }
+    }
+
+    file.close();
     return 0;
 }
+
+/*
+#include <bitset>
+
+int main() {
+    std::string text = "HelloWorld";
+    
+    for (char c : text) {
+        std::bitset<8> binaryChar(c);
+        std::cout << binaryChar << " ";
+    }
+    
+    std::cout << std::endl;
+    
+    return 0;
+}
+
+int numerAscii = 65; // Przykład: 65 odpowiada literze 'A' w ASCII
+
+char znak = static_cast<char>(numerAscii);
+
+std::cout << "Znak o numerze ASCII " << numerAscii << " to: " << znak << std::endl;
+
+*/
