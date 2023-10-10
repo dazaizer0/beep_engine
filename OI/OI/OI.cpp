dd0f084b0775ddb694025cd6b8c6a8ff9979ab24
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-10-10 16:18:05 +0200

    pointers

--- OI/OI/OI.cpp
+++ OI/OI/OI.cpp
@@ -1,6 +1,45 @@
 ﻿#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+template <typename T>
+class Vector2 {
+public:
+	struct Position {
+		T x;
+		T y;
+		T z;
+	};
+
+	Position position;
+
+	void echo_position() {
+		std::cout << "x: " << position.x << ", y: " << position.y << ", z: " << position.z << std::endl;
+	}
+};
+
+int main() {
+	// TEMPLATE CLASS
+	Vector2<float> vector2;
+	vector2.position.x = 13.5;
+	vector2.position.y = 7.25;
+	vector2.position.z = 3.75;
+
+	vector2.echo_position();
+
+	// POINTERS
+	int number = 256;
+	int* pointer = &number;
+	std::cout << pointer << " - " << *pointer << std::endl;
+
+	// DYNAMIC POINTERS
+	int* dynamic_int = new int;
+	*dynamic_int = 16;
+	std::cout << dynamic_int << " - " << *dynamic_int << std::endl;
+
+	// TABLE POINTERS
+	int tab[5] = { 0, 2, 4, 8, 16 };
+	int* tab_pointer = tab;
+	int element = tab_pointer[1];
+	std::cout << tab_pointer << " - " << element << std::endl;
+
+	return 0;
 }
