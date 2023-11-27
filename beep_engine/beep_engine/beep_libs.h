f406bd789a9620c7dd0201e56127242f011da48d
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-11-27 18:23:54 +0100

    v1.1.0

---
+++ beep_engine/beep_engine/beep_libs.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <iostream>
+#include <map>
+#include <chrono>
+#include <thread>
+#include <conio.h>
+#include <cstdlib>
+#include <fstream>
+
+#ifdef _WIN32
+#include <Windows.h>
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
